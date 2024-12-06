ff7429156a2fe8cefc3262a423951ba7a0fd769a
KiranPandit <kiranpandit1304@gmail.com>
2024-12-06 12:39:42 +0530

    First commit

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,27 @@
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public $user;
+
+    public function __construct($message, $user)
+    {
+        $this->message = $message;
+        $this->user = $user;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('chat');
+    }
+}
